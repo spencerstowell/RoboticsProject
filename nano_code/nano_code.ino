6608a3f26d63f5d38335724030c20de5e89a3567
spencerstowell <stowel22@byu.edu>
2024-01-28 19:55:55 -0700

    Added Eagle PCB & Schematic files

--- nano_code/nano_code.ino
+++ nano_code/nano_code.ino
@@ -1,22 +1,20 @@
+
 /*
- * cheapStepper_move.ino
+ * cheapStepper_newMoveTo.ino
  * ///////////////////////////////////////////
  * using CheapStepper Arduino library v.0.2.0
  * created by Tyler Henry, 7/2016
  * ///////////////////////////////////////////
  * 
  * This sketch illustrates the library's
- * "blocking" move functions -
- * i.e. the move will "pause" the arduino sketch
- * -- for non-blocking moves, see cheapStepper_newMoveTo.ino example
+ * "non-blocking" move functions -
+ * i.e. you can perform moves with the stepper over time
+ * while still running other code in your loop()
  * 
- * This sketch also shows how to set the RPM
- * and shows a few different types of move functions
- * - by steps or by degrees.
- * 
- * Blocking moves are useful if you need a specific RPM
- * but don't need your arduino to perform other functions
- * while the stepper is moving.
+ * This can be useful if your Arduino is multi-tasking,
+ * but be careful: if the other code in your loop()
+ * slows down your Arduino, the stepper motor may
+ * slow down or move with a stutter
  * 
  * //////////////////////////////////////////////////////
  */
@@ -25,112 +23,150 @@
 
 #include <CheapStepper.h>
 
-// next, declare the stepper
-// and connect pins 8,9,10,11 to IN1,IN2,IN3,IN4 on ULN2003 board
+using namespace std;
 
-// Declare 3 CheapStepper objects, counting down from pin 13 to pin 2
+// next, declare the stepper objects
 
-CheapStepper stepper0 (13, 12, 11, 10);
-CheapStepper stepper1 (9, 8, 7, 6);
-CheapStepper stepper2 (5, 4, 3, 2); 
- // let's create a boolean variable to save the direction of our rotation
-
-boolean moveClockwise = true;
+CheapStepper stepper0 (13,12,11,10); // stepper0 is connected to pins 8,9,10,11
+CheapStepper stepper1 (9,8,7,6); // stepper1 is connected to pins 8,9,10,11
+CheapStepper stepper2 (5,4,3,2); // stepper2 is connected to pins 8,9,10,11
 
 
+ // let's also create a boolean variable to save the direction of our rotation
+ // and a timer variable to keep track of move times
+
+bool moveClockwise = true;
+unsigned long moveStartTime = 0; // this will save the time (millis()) when we started each new move
+//float q[300][3] = {};
+
+// Joint angle array
 void setup() {
 
-  // let's set a custom speed of 20rpm (the default is ~16.25rpm)
-  
-  stepper0.setRpm(20); 
-  stepper1.setRpm(20);
-  stepper2.setRpm(20);
-  /* Note: CheapStepper library assumes you are powering your 28BYJ-48 stepper
-   * using an external 5V power supply (>100mA) for RPM calculations
-   * -- don't try to power the stepper directly from the Arduino
-   * 
-   * accepted RPM range: 6RPM (may overheat) - 24RPM (may skip)
-   * ideal range: 10RPM (safe, high torque) - 22RPM (fast, low torque)
-   */
+  // let's run the stepper at 12rpm (if using 5V power) - the default is ~16 rpm
 
-  // now let's set up a serial connection and print some stepper info to the console
+  stepper0.setRpm(12);
+  stepper1.setRpm(12);
+  stepper2.setRpm(12);
+
+  // let's print out the RPM to make sure the setting worked
   
-  Serial.begin(9600); Serial.println();
-  Serial.print(stepper0.getRpm()); // get the RPM of the stepper
-  Serial.print(" rpm = delay of ");
-  Serial.print(stepper0.getDelay()); // get delay between steps for set RPM
-  Serial.print(" microseconds between steps");
+  Serial.begin(9600);
+//  Serial.print("stepper RPM: "); Serial.print(stepper.getRpm());
   Serial.println();
-  
-  // stepper0.setTotalSteps(4076);
-  /* you can uncomment the above line if you think your motor
-   * is geared 63.68395:1 (measured) rather than 64:1 (advertised)
-   * which would make the total steps 4076 (rather than default 4096)
-   * for more info see: http://forum.arduino.cc/index.php?topic=71964.15
+
+  while (!Serial) {
+    delay(100); // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // and let's print the delay time (in microseconds) between each step
+  // the delay is based on the RPM setting:
+  // it's how long the stepper will wait before each step
+
+//  Serial.print("stepper delay (micros): "); Serial.print(stepper.getDelay());
+  Serial.println();
+
+  // now let's set up our first move...
+  // let's move a half rotation from the start point
+
+  // stepper0.newMoveTo(moveClockwise, 2048/4);
+  // stepper1.newMoveTo(moveClockwise, 2048/4);
+  // stepper2.newMoveTo(moveClockwise, 2048/4);
+
+  /* this is the same as: 
+   * stepper.newMoveToDegree(clockwise, 180);
+   * because there are 4096 (default) steps in a full rotation
    */
+  moveStartTime = millis(); // let's save the time at which we started this move
+  
 }
 
+float * processReceivedData(String data);
+float * q = {};
 void loop() {
 
-    // let's do a clockwise move first
+  if (Serial.available() > 0) {
+    
+    // Read the incoming data
+    String receivedData = Serial.readStringUntil('\n');
+    
+    // Process the received data (assuming it's comma-separated)
+    q = processReceivedData(receivedData);
+  }
+
+  // we need to call run() during loop() 
+  // in order to keep the stepper moving
+  // if we are using non-blocking moves
   
-    moveClockwise = true;
+  stepper0.run();
+  stepper1.run();
+  stepper2.run();
 
-    // let's move the stepper clockwise to position 2048
-    // which is 180 degrees, a half-turn (if using default of 4096 total steps)
-    
-    stepper0.moveTo (moveClockwise, 2048/2);
-    stepper1.moveTo (moveClockwise, 2048/2);
-    stepper2.moveTo (moveClockwise, 2048/2);
+  ////////////////////////////////
+  // now the stepper is moving, //
+  // let's do some other stuff! //
+  ////////////////////////////////
 
-    // now let's print the stepper position to the console
+  // let's check how many steps are left in the current move:
+  
+  int stepsLeft = stepper0.getStepsLeft();
+
+  // if the current move is done...
+  
+  if (stepsLeft == 0){
+
+    // let's print the position of the stepper to serial
     
-    Serial.print("step position: ");  
-    Serial.print(stepper0.getStep()); // get the current step position
-    Serial.print(" / 4096");
+    Serial.print("stepper position: "); Serial.print(stepper0.getStep());
     Serial.println();
 
-    // now let's wait one second
+    // and now let's print the time the move took
+
+    unsigned long timeTook = millis() - moveStartTime; // calculate time elapsed since move start
+    Serial.print("move took (ms): "); Serial.print(timeTook);
+    Serial.println(); Serial.println();
     
-    delay(1000); // wait a sec
-
-    // and now let's move another 90 degrees (a quarter-turn) clockwise
-
-    stepper0.moveDegrees (moveClockwise, 45);
-    stepper1.moveDegrees (moveClockwise, 45);
-    stepper2.moveDegrees (moveClockwise, 45);
-    // stepper0.moveDegreesCW (90); <--- another way to do a clockwise 90 degree turn
-
-    // let's print the stepper position to the console again
+    // let's start a new move in the reverse direction
     
-    Serial.print("step position: ");
-    Serial.print(stepper0.getStep());
-    Serial.print(" / 4096");
-    Serial.println();
-
-    // and wait another second
+    moveClockwise = !moveClockwise; // reverse direction
     
-    delay(1000);
+    stepper0.newMoveDegrees (moveClockwise, 45); // move 180 degrees from current position
+    stepper1.newMoveDegrees (moveClockwise, 45); // move 180 degrees from current position
+    stepper2.newMoveDegrees (moveClockwise, 45); // move 180 degrees from current position
 
-    // ok, now let's reverse directions (to counter-clockwise)
-    
-    moveClockwise = false;
+    moveStartTime = millis(); // reset move start time
 
-    // and move back to the start position (0 degree)
-   
-    stepper0.moveToDegree (moveClockwise, 0);
-    stepper1.moveToDegree (moveClockwise, 0);
-    stepper2.moveToDegree (moveClockwise, 0);
-    // moveClockwise is now false, so move counter-clockwise back to start
+  }
 
-    // let's print the position to the console once again
-    
-    Serial.print("step position: ");
-    Serial.print(stepper0.getStep());
-    Serial.print(" / 4096");
-    Serial.println();
+}
 
-    // and wait another second before starting loop() over
-    
-    delay(1000);  
+float * processReceivedData(String data) {
+  // Split the received data into individual elements
+  String values[900];  // Assuming maximum of 300 rows and 3 columns
+  int i = 0;
+
+  while (data.length() > 0) {
+    int commaIndex = data.indexOf(',');
+    if (commaIndex > 0) {
+      values[i] = data.substring(0, commaIndex);
+      data = data.substring(commaIndex + 1);
+    }
+    else {
+      values[i] = data;
+      data = "";
+    }
+    i++;
+  }
+
+  // Convert string values to floats and reconstruct the original 2D array
+  float receivedArray[300][3];
+
+  for (int row = 0; row < 300; row++) {
+    for (int col = 0; col < 3; col++) {
+      receivedArray[row][col] = values[row * 3 + col].toFloat();
+    }
+  }
+
+  // Now you can use the 'receivedArray' for further processing
+  // Add your code here to use the received data as needed
+  return receivedArray;
 }
